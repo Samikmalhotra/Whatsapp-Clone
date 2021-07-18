30879ae2136a80055ed7968c074bd82e2e743bf1
Samikmalhotra <samikmalhotra66@gmail.com>
2021-07-19 03:51:59 +0530

    updated chat

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../CSS/chat.css'
+import {Avatar, IconButton} from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 
 const Chat = () => {
     return (
         <div className="chat">
-            I am a chat
+            <div className="chat-header">
+                <Avatar/>
+                <div className="chat-header-info">
+                    <h3>Room Name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+                <div className="chat-header-right">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>  
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>                  
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
